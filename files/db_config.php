3a5cd7b29b710fbc25abfe15edeaa4527a3b7fdd
Jordi Canals <jordicanalsros@gmail.com>
2014-08-26 01:15:25 +0200

    Files php & sql

---
+++ files/db_config.php
@@ -0,0 +1,13 @@
+<?php
+	
+	/* db_config.php */
+ 
+	/*
+	 * All database connection variables
+	 */
+	 
+	define('DB_USER', "canalsprojects"); 		// db user
+	define('DB_PASSWORD', "1q2w3e"); 			// db password
+	define('DB_DATABASE', "canalsprojects"); 	// database name
+	define('DB_SERVER', "hl177.dinaserver.com"); // db server
+?>
