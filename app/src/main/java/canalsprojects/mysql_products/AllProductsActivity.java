095b214d9048188b768c8793f0f152759dac641b
Jordi Canals <jordicanalsros@gmail.com>
2014-08-31 04:22:04 +0200

    Filtros

--- app/src/main/java/canalsprojects/mysql_products/AllProductsActivity.java
+++ app/src/main/java/canalsprojects/mysql_products/AllProductsActivity.java
@@ -5,6 +5,7 @@ package canalsprojects.mysql_products;
  */
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -26,21 +27,21 @@ import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AllProductsActivity extends ListActivity {
 
-    // Progress Dialog
-    //private ProgressDialog pDialog;
-
-    // Creating JSON Parser object
     private JSONParser jParser = new JSONParser();
     private CustomArrayAdapter adapter = null;
     private SearchView search;
+    private Spinner spinner1;
+    private Spinner spinner2;
 
-    // url to get all products list
+    // url to get aljava.lang.Stringl products list
     private static String url_all_products = "http://bd.mumus.es/get_all_products.php";
 
     // JSON Node names
@@ -48,13 +49,20 @@ public class AllProductsActivity extends ListActivity {
     private static final String TAG_PRODUCTS = "products";
     private static final String TAG_START = "start";
     private static final String TAG_END = "end";
+    private static final String TAG_SORT = "sort";
+    private static final String TAG_TSORT = "tsort";
 
-    // products JSONArray
     private JSONArray products = null;
     private boolean loadingInfo = false;
     private boolean MoreInfo = true;
-    String query = "";
-    String query2 = "";
+    private String query = "";
+
+    // Sort
+    private String[] sortList;
+    private String[] typeSortList;
+
+    private int sort = 0;
+    private int typeSort = 0;
 
 
     @Override
@@ -62,16 +70,15 @@ public class AllProductsActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_products);
 
-        // Get the intent, verify the action and get the query
-        /*Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.d("Query:", query);
-            this.query = query;
-        }*/
+        setupSpinners();
+        setupListView();
+
+    }
+
+    private void setupListView() {
 
         // Loading products in List view
-        new LoadAllProducts(query).execute(0);
+        new LoadAllProducts(query).execute(0, sort, typeSort);
 
         // Get listview
         ListView lv = getListView();
@@ -112,11 +119,88 @@ public class AllProductsActivity extends ListActivity {
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
                 int lastItem = firstVisibleItem + visibleItemCount;
                 if ((lastItem == totalItemCount) && (totalItemCount != 0) && !loadingInfo && MoreInfo) {
-                    new LoadAllProducts(query).execute(lastItem);
+                    new LoadAllProducts(query).execute(lastItem, sort, typeSort);
                 }
             }
         });
 
+    }
+
+    private void setupSpinners() {
+
+        sortList = getResources().getStringArray(R.array.sort_list);
+        typeSortList= getResources().getStringArray(R.array.type_sort_list);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, sortList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(dataAdapter);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                sort = position;
+                if (adapter != null) {
+                    adapter.clear();
+                    new LoadAllProducts(query).execute(0, sort, typeSort);
+                }
+                //SortListVier(sort, typeSort);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, typeSortList);
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter2);
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                typeSort = position;
+                if (adapter != null) {
+                    adapter.clear();
+                    new LoadAllProducts(query).execute(0, sort, typeSort);
+                }
+                //SortListVier(sort, typeSort);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+    }
+
+    private void SortListVier(int sort, int typeSort) {
+        if (adapter == null) return;
+        switch (sort){
+            case 0:
+                if (typeSort == 0) {
+                    adapter.sort(PriceAscComparator);
+                } else if (typeSort == 1) {
+                    adapter.sort(NameAscComparator);
+                }
+                break;
+            case 1:
+                if (typeSort == 0) {
+                    adapter.sort(PriceDescComparator);
+                } else if (typeSort == 1) {
+                    adapter.sort(NameDescComparator);
+                }
+                break;
+        }
     }
 
     @Override
@@ -136,7 +220,7 @@ public class AllProductsActivity extends ListActivity {
                 query = q;
                 search.onActionViewCollapsed();
                 adapter.clear();
-                new LoadAllProducts(q).execute(0);
+                new LoadAllProducts(q).execute(0, sort, typeSort);
                 return true;
             }
 
@@ -167,7 +251,7 @@ public class AllProductsActivity extends ListActivity {
     /**
      * Background Async Task to Load all product by making HTTP Request
      * */
-    class LoadAllProducts extends AsyncTask<Integer, String, Integer> {
+    private class LoadAllProducts extends AsyncTask<Integer, String, Integer> {
 
         String query;
         ArrayList<Product> productsList;
@@ -190,10 +274,9 @@ public class AllProductsActivity extends ListActivity {
             // Hashmap for ListView
             productsList = new ArrayList<Product>();
 
-                //add the footer before adding the adapter, else the footer will not load!
-                loadMoreView = ((LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_footer, null, false);
-                lv.addFooterView(loadMoreView);
-
+            //add the footer before adding the adapter, else the footer will not load!
+            loadMoreView = ((LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_footer, null);
+            lv.addFooterView(loadMoreView);
         }
 
         /**
@@ -208,6 +291,8 @@ public class AllProductsActivity extends ListActivity {
             params.add(new BasicNameValuePair(TAG_START, args[0].toString()));
             params.add(new BasicNameValuePair(TAG_END, String.valueOf(end)));
             params.add(new BasicNameValuePair("q", query));
+            params.add(new BasicNameValuePair(TAG_SORT, args[1].toString()));
+            params.add(new BasicNameValuePair(TAG_TSORT, args[2].toString()));
 
             // getting JSON string from URL
             JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
@@ -245,8 +330,7 @@ public class AllProductsActivity extends ListActivity {
          * After completing background task Dismiss the progress dialog
          * **/
         protected void onPostExecute(final Integer lastItem) {
-            // dismiss the dialog after getting all products
-//            pDialog.dismiss();
+
             // updating UI from Background Thread
             runOnUiThread(new Runnable() {
                 public void run() {
@@ -264,5 +348,42 @@ public class AllProductsActivity extends ListActivity {
             loadingInfo = false;
         }
     }
+
+    // Comparator for Ascending Order
+    public static Comparator<Product> PriceAscComparator = new Comparator<Product>() {
+        public int compare(Product p1, Product p2) {
+            float price1 = Float.parseFloat(p1.getPrice());
+            float price2 = Float.parseFloat(p2.getPrice());
+            return Float.compare(price1, price2);
+        }
+    };
+
+    //Comparator for Descending Order
+    public static Comparator<Product> PriceDescComparator = new Comparator<Product>() {
+        public int compare(Product p1, Product p2) {
+            float price1 = Float.parseFloat(p1.getPrice());
+            float price2 = Float.parseFloat(p2.getPrice());
+            return Float.compare(price2, price1);
+        }
+    };
+
+    // Comparator for Ascending Order
+    public static Comparator<Product> NameAscComparator = new Comparator<Product>() {
+        public int compare(Product p1, Product p2) {
+            String name1 = p1.getName();
+            String name2 = p2.getName();
+            return name1.compareToIgnoreCase(name2);
+        }
+    };
+
+    //Comparator for Descending Order
+    public static Comparator<Product> NameDescComparator = new Comparator<Product>() {
+        public int compare(Product p1, Product p2) {
+            String name1 = p1.getName();
+            String name2 = p2.getName();
+            return name2.compareToIgnoreCase(name1);
+        }
+    };
+
 
 }
